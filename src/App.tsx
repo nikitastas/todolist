5213f6a69a8f7e3047d429a9c6bbee9fee4a303c
nikitastas <nikitastas1694@gmail.com>
2024-06-08 00:00:35 +0300

    Add useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {Todolist} from './Todolist';
+import {useState} from 'react';
 
 type Task = {
     id: number
@@ -8,22 +9,23 @@ type Task = {
 }
 
 export function App() {
-    let tasks1: Array<Task> = [
+    const [tasks, setTasks] = useState<Array<Task>>([
         { id: 1, title: 'HTML&CSS', isDone: true },
         { id: 2, title: 'JS', isDone: true },
         { id: 3, title: 'ReactJS', isDone: false },
         { id: 4, title: 'Redux', isDone: false },
         { id: 5, title: 'Typescript ', isDone: false },
         { id: 6, title: 'RTK query', isDone: false },
-    ]
+    ])
 
     const removeTask = (taskId: number) => {
-        tasks1 = tasks1.filter(task => task.id !== taskId);
+        const filteredTasks = tasks.filter(task => task.id !== taskId)
+        setTasks(filteredTasks)
     }
 
     return (
         <div className="App">
-            <Todolist title = {"What to learn"} tasks={tasks1} date={'06.06.2024'} removeTask={removeTask}/>
+            <Todolist title = {"What to learn"} tasks={tasks} date={'06.06.2024'} removeTask={removeTask}/>
         </div>
     );
 }
