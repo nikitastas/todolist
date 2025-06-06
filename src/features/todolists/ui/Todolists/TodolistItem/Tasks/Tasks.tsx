db893374b8d2b95f277469d8d36ac8dab1e68ced
nikitastas <nikitastas1694@gmail.com>
2025-06-07 01:28:48 +0300

    feat: add error handling on useQuery and useMutation level

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -4,6 +4,9 @@ import type { DomainTodolist } from "@/features/todolists/model/todolists-slice"
 import List from "@mui/material/List"
 import { TaskItem } from "./TaskItem/TaskItem"
 import { TasksSkeleton } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksSkeleton/TasksSkeleton.tsx"
+import { useAppDispatch } from "@/common/hooks"
+import { useEffect } from "react"
+import { setAppErrorAC } from "@/app/app-slice.ts"
 
 type Props = {
   todolist: DomainTodolist
@@ -12,7 +15,19 @@ type Props = {
 export const Tasks = ({ todolist }: Props) => {
   const { id, filter } = todolist
 
-  const { data, isLoading } = useGetTasksQuery(id)
+  const { data, isLoading, error } = useGetTasksQuery(id)
+
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    if (!error) return
+    if ("status" in error) {
+      const errMsg = "error" in error ? error.error : JSON.stringify(error.data)
+      dispatch(setAppErrorAC({ error: errMsg }))
+    } else {
+      dispatch(setAppErrorAC({ error: error.message || "Some error occurred" }))
+    }
+  }, [error])
 
   let filteredTasks = data?.items
   if (filter === "active") {
